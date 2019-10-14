2bd6470f6859a33283b4ffb81918a93c387acbb8
jeremy.lincoln <jeremy.lincoln@bisonline.com>
2019-10-14 08:32:16 -0400

    Added in todos/{id} route, new layouts folder to help with reuse code

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodosController extends Controller
 {
     public function index()
     {
-        return view('todos.index');
+        //fetch all todos from database
+        //display them in the todos.index page
+
+        // $todos = Todo::all();
+
+        // return view('todos.index')->with('todos', $todos);
+
+        //can shrink the above to this
+        return view('todos.index')->with('todos', Todo::all());
+    }
+
+    public function show($todoId)
+    {
+        //dd() is the same as die(var_dump())
+        //dd($todoId);
+        $todo = Todo::find($todoId);
+
+        return view('todos.show')->with('todo', $todo);
     }
 }
